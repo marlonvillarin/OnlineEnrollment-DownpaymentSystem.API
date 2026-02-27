9f4e9503e1ed5eebfa93ab182ef0765bbe028403
Banjo <banjo.singcol@rhinobill.com>
2026-02-27 20:51:38 +0800

    added email and contact

--- OnlineEnrollment&DownpaymentSystem.API/Model/Student.cs
+++ OnlineEnrollment&DownpaymentSystem.API/Model/Student.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public string Email { get; set; }
+        public int ContactNumber { get; set; }
     }
 }
